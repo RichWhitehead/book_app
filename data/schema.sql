be67e26bee13d6fcfba298cadc57a8a3ce07c323
Richard Whitehead <damian253@me.com>
2020-05-04 21:39:38 -0700

    updates

--- data/schema.sql
+++ data/schema.sql
@@ -5,6 +5,7 @@ author VARCHAR (255),
 title VARCHAR (255),
 isbn VARCHAR (255),
 image_url VARCHAR (255),
+amount VARCHAR (255),
 description VARCHAR (255),
 bookshelf VARCHAR (255)
 )
--- server.js
+++ server.js
@@ -2,16 +2,16 @@
 
 require('dotenv').config();
 const express = require('express');
-const pg =require('pg');
+const pg = require('pg');
 const superagent = require('superagent');
 const PORT = process.env.PORT || 3000;
 const app = express();
+const client = new pg.Client (process.env.DATABASE_URL);
+app.set ('view engine', 'ejs');
 app.use( express.urlencoded({extended:true }));
 app.use( express.static('./www'));
 
 
-const client = new pg.Client (process.env.DATABASE_URL);
-app.set ('view engine', 'ejs');
 
 
 
@@ -28,12 +28,13 @@ app.post('/city', (request, response) => {
   response.status(200).send(request.body.article);
 });
 
+
 // This will force an error
 app.get('/badthing', (request,response) => {
   throw new Error('WTF???');
 });
 
-
+//Routes
 app.get ('/', (request, response) => {
   response.status(200).render('pages/search-form');
 });
@@ -63,36 +64,47 @@ function Book(data){
   this.amount = data.saleInfo.listPrice ? data.saleInfo.listPrice.amount : ' Unknown.';
 }
 
-// Query Database for all to do items
-
-app.get('/', (request, response) => {
+//Saving books
+app.get('/index', (request, response) => {
   const SQL = 'SELECT * FROM books';
   client.query(SQL)
     .then (results => {
-      response.status(200).render('pages/index.ejs', {books:results.rows});
+      response.status(200).render('pages/index', {books:results.rows});
     })
     .catch ( error => {
       throw new Error('Oh No! There is an Error!', error);
     });
 });
 
-// // add new book to database
-// app.post('/add', (request, response) => {
-//   console.log(request.body);
-//   const SQL = `
-//     INSERT INTO books (author, title, isbn, image_url, _description, bookshelf, amount)
-//     VALUES ($1, $2, $3, $4, $5, $6, $7)
-//   `;
-//   const VALUES = [
-//     request.body.author,
-//     request.body.title,
-//     request.body.isbn,
-//     request.body.image_url,
-//     request.body._description,
-//     request.body.bookshelf,
-//     request.body.amount
-//   ];
-//   client.query(SQL, VALUES)
+// add new book to database
+app.post('/add', (request, response) => {
+  console.log('You are here', request);
+  let SQL = `
+    INSERT INTO books (author, title, isbn, image_url, description, bookshelf, amount)
+    VALUES ($1, $2, $3, $4, $5, $6, $7)`;
+
+  let VALUES = [
+    request.body.author,
+    request.body.title,
+    request.body.isbn,
+    request.body.image_url,
+    request.body.description,
+    request.body.bookshelf,
+    request.body.amount
+  ];
+  client.query(SQL, VALUES)
+    .then( () => {
+      response.status(200).render('pages/index');
+    })
+    .catch( error => {
+      console.error(error.message);
+    });
+});
+
+// delete book from database
+// app.post('/delete',(request,response) => {
+//   const SQL = 'DELETE FROM books WHERE id=$1';
+//   client.query(SQL)
 //     .then( () => {
 //       response.status(200).redirect('/');
 //     })
@@ -101,16 +113,21 @@ app.get('/', (request, response) => {
 //     });
 // });
 
+// Force error
+app.get('/error', () => {
+  throw new Error('You Broke It!');
+});
+
   
   // 404 Handler
   app.use('*', (request, response) => {
-    console.log(request);
+    // console.log(request);
     response.status(404).send(`Can't Find ${request.pathname}`);
   });
   
   // Error Handler
   app.use( (err,request,response,next) => {
-    console.error(err);
+    // console.error(err);
     response.status(500).send(err.message);
   });
 
